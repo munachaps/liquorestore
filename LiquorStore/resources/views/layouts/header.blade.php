31e18f24fad25f2f7865de9b3a681928f2726c6f
Munashe Chapata <munachaps@gmail.com>
2024-01-23 17:43:46 +0200

    Updated landig page version 1

---
+++ LiquorStore/resources/views/layouts/header.blade.php
@@ -0,0 +1,62 @@
+<div class="sticky-wrapper">
+    <div class="menu-area">
+        <div class="container th-container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-auto">
+                    <div class="header-logo">
+                        <a href="index.html"><img src="assets/img/logo-white.svg" alt="Pizzan" /></a>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <nav class="main-menu d-none d-lg-inline-block">
+                        <ul>
+                            <li class="menu-item-has-children">
+                                <a href="">BEERS AND CIDERS</a>
+                                <ul class="sub-menu"></ul>
+                           </li>
+            <li class="menu-item-has-children">
+            <a href="#">SPIRITS</a>
+                    <ul class="sub-menu">
+                        </ul>
+            </li>
+            <li class="menu-item-has-children">
+                 <a href="#">WINES</a>
+                    <ul class="sub-menu">
+                    </ul>
+            </li>
+            <li class="menu-item-has-children">
+                <a href="#">MIXERS</a>
+                   <ul class="sub-menu">
+                   </ul>
+           </li>
+           <li class="menu-item-has-children">
+            <a href="#">SHOOTERS</a>
+               <ul class="sub-menu">
+               </ul>
+       </li>
+       <li class="menu-item-has-children">
+        <a href="#">DRINKS AND JUICES</a>
+           <ul class="sub-menu">
+           </ul>
+        </li>
+            </nav>
+                </div>
+                <div class="col-auto">
+                    <div class="header-button">
+                        <button type="button" class="simple-icon searchBoxToggler">
+<i class="far fa-search"></i>
+</button>
+                        <button type="button" class="simple-icon sideMenuToggler">
+<i class="far fa-cart-shopping"></i>
+<span class="badge"></span>
+</button>
+                     
+                        <button type="button" class="th-menu-toggle d-inline-block d-lg-none">
+<i class="far fa-bars"></i>
+</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
